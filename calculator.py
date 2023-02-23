e5b0d3934712b560e9d3a6b489777975079f921b
Daniel Jovevski <121831389+Daniel-AnTra@users.noreply.github.com>
2023-02-23 18:27:53 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -37,23 +37,6 @@ while choice != 0:
             yesno = str(input("Do you want new caluclation : press yes/no "))  
             
         if  yesno == "yes":
-            while choice != 0:
-                x = float(input("Enter the first number: ")) 
-                y = float(input("Enter the second number: "))
-                print ("1. Addition")
-                print ("2. Substraction")
-                print ("3. Multiplication")
-                print ("4. Division")
-                choice = int(input("Enter your choice of operation : "))
-                if choice == 1:
-                    print("The result is " + str(obj1.addition()))
-                elif choice == 2:
-                    print("The result is " + str(obj1.substraction()))
-                elif choice == 3:
-                    print("The result is " + str(obj1.multiplication()))
-                elif choice == 4:
-                    print("The result is " + str(obj1.division()))     
-                else:
-                    print("Invalid choice. Choose one of the operations on the list")
+            continue
         elif yesno == "no":
             break
