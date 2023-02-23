1226f3ba912dce765995e3cf5cfb83de3ef4f96f
Daniel Jovevski <121831389+Daniel-AnTra@users.noreply.github.com>
2023-02-23 17:47:15 +0100

    Homework No.04 - Calculator and Code Combat Game
    Hello, here is screenshot of the passed 5 levels of codecombat game, two screenshots of the calculator (the code in python and the output) and the actual python file of the calculator.

---
+++ calculator.py
@@ -0,0 +1,56 @@
+class calculator:
+    def addition(self):
+        return (x + y)
+    def substraction(self):
+        return (x - y)
+    def multiplication(self):       
+        return (x * y)
+    def division(self):
+        return (x / y) 
+
+obj1 = calculator()
+choice = 1
+while choice != 0:
+        x = int(input("Enter the first number: ")) 
+        y = int(input("Enter the second number: "))
+        print ("1. Addition")
+        print ("2. Substraction")
+        print ("3. Multiplication")
+        print ("4. Division")
+        choice = int(input("Enter your choice of operation : "))
+        if choice == 1:
+            print("The result is " + str(obj1.addition()))
+            yesno = str(input("Do you want new caluclation : press y/n "))
+        elif choice == 2:
+            print("The result is " + str(obj1.substraction()))
+            yesno = str(input("Do you want new caluclation : press y/n "))
+        elif choice == 3:
+            print("The result is " + str(obj1.multiplication()))
+            yesno = str(input("Do you want new caluclation : press y/n "))
+        elif choice == 4:
+            print("The result is " + str(obj1.division()))   
+            yesno = str(input("Do you want new caluclation : press y/n "))  
+        else:
+            print("Invalid choice. Choose one of the operations on the list")
+            
+        if  yesno == "y":
+            while choice != 0:
+                x = int(input("Enter the first number: ")) 
+                y = int(input("Enter the second number: "))
+                print ("1. Addition")
+                print ("2. Substraction")
+                print ("3. Multiplication")
+                print ("4. Division")
+                choice = int(input("Enter your choice of operation : "))
+                if choice == 1:
+                    print("The result is " + str(obj1.addition()))
+                elif choice == 2:
+                    print("The result is " + str(obj1.substraction()))
+                elif choice == 3:
+                    print("The result is " + str(obj1.multiplication()))
+                elif choice == 4:
+                    print("The result is " + str(obj1.division()))     
+                else:
+                    print("Invalid choice. Choose one of the operations on the list")
+        elif yesno == "n":
+            break
