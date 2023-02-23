65222d2ea6f15da623cf402dbddbdc25de71368d
Daniel Jovevski <121831389+Daniel-AnTra@users.noreply.github.com>
2023-02-23 18:36:49 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -13,13 +13,14 @@ class calculator:
 obj1 = calculator()
 choice = 1
 while choice != 0:
-        x = float(input("Enter the first number: ")) 
-        y = float(input("Enter the second number: "))
         print ("1. Addition")
         print ("2. Substraction")
         print ("3. Multiplication")
         print ("4. Division")
         choice = int(input("Enter your choice of operation : "))
+        x = float(input("Enter the first number: ")) 
+        y = float(input("Enter the second number: "))
+        
         if choice == 1:
             print("The result is " + str(obj1.addition()))
             yesno = str(input("Do you want new caluclation : press yes/no "))
