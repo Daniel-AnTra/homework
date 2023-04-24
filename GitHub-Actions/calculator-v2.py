52891eae853bf730684ccc8d1b91a231432e9175
Daniel Jovevski <danieljovevski92@gmail.com>
2023-04-24 22:19:23 +0200

    Update calculator-v2.py

--- GitHub-Actions/calculator-v2.py
+++ GitHub-Actions/calculator-v2.py
@@ -1,39 +1,47 @@
 import argparse
+import random
 
 class Calculator:
-    def addition(self, x, y):
+    def add(self, x, y):
         return x + y
-
-    def subtraction(self, x, y):
+    def subtract(self, x, y):
         return x - y
-
-    def multiplication(self, x, y):
+    def multiply(self, x, y):
         return x * y
-
-    def division(self, x, y):
-        if y != 0:
-            return x / y
-        else:
-            print("You can't divide by 0")
-            return None
+    def divide(self, x, y):
+        if y == 0:
+            raise ValueError("Cannot divide by zero")
+        return x / y
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='A simple calculator.')
-    parser.add_argument('operation', choices=['add', 'subtract', 'multiply', 'divide'], help='The operation to perform.')
-    parser.add_argument('x', type=float, help='The first number.')
-    parser.add_argument('y', type=float, help='The second number.')
+    parser = argparse.ArgumentParser(description="A simple calculator")
+    parser.add_argument("operation", choices=["add", "subtract", "multiply", "divide"], help="operation to perform")
+    parser.add_argument("x", type=float, help="first operand")
+    parser.add_argument("y", type=float, help="second operand")
+    
     args = parser.parse_args()
-
+    
     calculator = Calculator()
-
-    if args.operation == 'add':
-        result = calculator.addition(args.x, args.y)
-    elif args.operation == 'subtract':
-        result = calculator.subtraction(args.x, args.y)
-    elif args.operation == 'multiply':
-        result = calculator.multiplication(args.x, args.y)
-    elif args.operation == 'divide':
-        result = calculator.division(args.x, args.y)
-
-    if result is not None:
-        print(f"The result is {result}")
+    
+    # Generate random values for operation and operands
+    operations = ["add", "subtract", "multiply", "divide"]
+    op = random.choice(operations)
+    x = random.randint(1, 10)
+    y = random.randint(1, 10)
+    
+    # Perform the selected operation
+    if op == "add":
+        result = calculator.add(x, y)
+    elif op == "subtract":
+        result = calculator.subtract(x, y)
+    elif op == "multiply":
+        result = calculator.multiply(x, y)
+    else:
+        try:
+            result = calculator.divide(x, y)
+        except ValueError as e:
+            print(e)
+            exit(1)
+    
+    # Print the result
+    print(f"{op}({x}, {y}) = {result}")
